b886f157a45d45c4d21fd320d5a39a1b28d306e0
metehan-ugus <ugusmetehan@gmail.com>
2025-01-29 01:58:32 +0300

    Collectibles Update

--- Assets/_Unity Essentials/Scripts/Collectible.cs
+++ Assets/_Unity Essentials/Scripts/Collectible.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Collectible : MonoBehaviour
 {
     public float rotationSpeed = 100.0f;
+    public GameObject collectedEffect;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,5 +14,14 @@ public class Collectible : MonoBehaviour
     void Update()
     {
         transform.Rotate(0,rotationSpeed * Time.deltaTime,0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Instantiate(collectedEffect,transform.position,transform.rotation);
+            Destroy(gameObject);
+        }
     }
 }
